a1f40ca5c97dc11077c018124a3301ca5a2ebbb2
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2022-09-06 10:21:47 -0300

    parte de programa concluído

---
+++ src/br/senai/sp/jandira/model/AppTabuada.java
@@ -0,0 +1,29 @@
+package br.senai.sp.jandira.model;
+
+public class AppTabuada {
+
+	public static void main(String[] args) {
+	//	
+//		Tabuada tabuada = new Tabuada();
+//		tabuada.multiplicando = 139;
+//		tabuada.minimoMultiplicador = 120;
+//		tabuada.maximoMultiplicador = 1000;
+//		
+//		String[] resultado = tabuada.getTabuada();
+//		
+//		
+//		int contador = 0;
+//		
+//		while (contador < resultado.length) {
+//			System.out.println(resultado[contador]);
+//			contador++;
+//			
+//		}
+//		
+//		System.out.println("FIM");
+//		
+//	
+
+	}
+
+}
