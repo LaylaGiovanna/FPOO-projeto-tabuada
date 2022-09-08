787a6cc6414d0d05173767761162afa30b8b61e2
Layla Giovanna <22282096@CFP127ALUNO.LOCAL>
2022-09-08 11:33:47 -0300

    quase terminando

--- src/br/senai/sp/jandira/gui/FrameTabuada.java
+++ src/br/senai/sp/jandira/gui/FrameTabuada.java
@@ -1,6 +1,5 @@
 package br.senai.sp.jandira.gui;
 
-
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
@@ -10,26 +9,34 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+//import javax.swing.JList;
+//import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 import br.senai.sp.jandira.Tabuada;
-import br.senai.sp.jandira.model.AppTabuada;
-
 
 public class FrameTabuada {
-	
+
 	public String titulo;
-	public int minimoMultiplicador;
-	public int maximoMultiplicador;
-	
+	public String subtitulo;
 	public int altura;
 	public int largura;
 	public Color corDoTexto;
 	public Font fonteDoTitulo;
 	public Font fonteDosLabels;
+	public Font fonteDosBotoes;
 	public Color corDeFundoDaTela;
 	public Color corDoBotaoLimpar;
 	public Color corDoBotaoCalcular;
+	public Color corDoTextoBotao;
+	public Color corDoTitulo;
+	public Color corDoSubtitulo;
+	public Color corDoJlist;
+	public LineBorder corDaBorda;
+
 
 	public void criarTela() {
 
@@ -41,92 +48,116 @@ public class FrameTabuada {
 		tela.setSize(largura, altura);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setLayout(null);
-		
 
 //		obter a instância do painel de conteúdo (container)
 		Container painel = tela.getContentPane();
 		painel.setBackground(corDeFundoDaTela);
-		
+
+//		título tabuada e subtitulo
+		JLabel labelTitulo = new JLabel();
+		labelTitulo.setText("Tabuada 1.0");
+		labelTitulo.setBounds(100, 1, 200, 50);
+		labelTitulo.setFont(fonteDoTitulo);
+		labelTitulo.setForeground(corDoTitulo);
+
+		JLabel labelSubtitulo = new JLabel();
+		labelSubtitulo.setText(
+				"<html>Com a tabuada 1.0 os seus problemas acabaram. Calcule<br/>a tabuada que desejar em segundos!</html>");
+		labelSubtitulo.setBounds(100, 50, 600, 40);
+		labelSubtitulo.setFont(fonteDosLabels);
+		labelSubtitulo.setForeground(corDoSubtitulo);
+
 //		Multiplicando
 		JLabel labelMultiplicando = new JLabel();
 		labelMultiplicando.setText("Multiplicando:");
-		labelMultiplicando.setBounds(30, 40, 100, 30);
+		labelMultiplicando.setBounds(110, 80, 187, 40);
 		labelMultiplicando.setFont(fonteDosLabels);
 
 		JTextField textFieldMultiplicando = new JTextField();
-		textFieldMultiplicando.setBounds(30, 70, 300, 30);
-		
+		textFieldMultiplicando.setBounds(360, 80, 187, 40);
+		textFieldMultiplicando.setBorder(corDaBorda);
+
 //		Minimo Multiplicador
 		JLabel labelMinimoMultiplicador = new JLabel();
-		labelMinimoMultiplicador.setText("Mínimo MultiplicadorPeso:");
-		labelMinimoMultiplicador.setBounds(30, 110, 300, 30);
+		labelMinimoMultiplicador.setText("Mínimo Multiplicador:");
+		labelMinimoMultiplicador.setBounds(110, 130, 187, 40);
 		labelMinimoMultiplicador.setFont(fonteDosLabels);
 
 		JTextField textFieldMinimoMultiplicador = new JTextField();
-		textFieldMinimoMultiplicador.setBounds(30, 140, 300, 30);
-		
+		textFieldMinimoMultiplicador.setBounds(360, 130, 187, 40);
+		textFieldMinimoMultiplicador.setBorder(corDaBorda);
+
 //		Maximo Multiplicador
 		JLabel labelMaximoMultiplicador = new JLabel();
 		labelMaximoMultiplicador.setText("Máximo Multiplicador:");
-		labelMaximoMultiplicador.setBounds(30, 180, 300, 30);
+		labelMaximoMultiplicador.setBounds(110, 210, 187, 40);
 		labelMaximoMultiplicador.setFont(fonteDosLabels);
 
 		JTextField textFieldMaximoMultiplicador = new JTextField();
-		textFieldMaximoMultiplicador.setBounds(30, 210, 300, 30);
-		
+		textFieldMaximoMultiplicador.setBounds(360, 210, 187, 40);
+		textFieldMaximoMultiplicador.setBorder(corDaBorda);
+
 //		Resultado
 		JLabel labelResultado = new JLabel();
 		labelResultado.setText("Resultado:");
-		labelResultado.setBounds(30, 320, 300, 30);
+		labelResultado.setBounds(30, 340, 187, 40);
 		labelResultado.setFont(fonteDosLabels);
-		
+
 //		botão Calcular e botão Limpar
 		JButton buttonCalcular = new JButton();
 		buttonCalcular.setText("Calcular");
-		buttonCalcular.setBounds(400, 280, 160, 30);
-		buttonCalcular.setForeground(corDoTexto);
+		buttonCalcular.setBounds(110, 280, 220, 50);
+		buttonCalcular.setForeground(corDoTextoBotao);
 		buttonCalcular.setBackground(corDoBotaoCalcular);
-		
+		buttonCalcular.setFont(fonteDosBotoes);
+
 		JButton buttonLimpar = new JButton();
-		buttonLimpar.setText("Calcular");
-		buttonLimpar.setBounds(400, 280, 160, 30);
-		buttonLimpar.setForeground(corDoTexto);
+		buttonLimpar.setText("Limpar");
+		buttonLimpar.setBounds(350, 280, 180, 50);
+		buttonLimpar.setForeground(corDoTextoBotao);
 		buttonLimpar.setBackground(corDoBotaoLimpar);
-		
-//		eventos de click  botão calcular
+		buttonLimpar.setFont(fonteDosBotoes);
+
+//		caixa do Jlist
+		JList lista = new JList();
+		JScrollPane scroll = new JScrollPane(lista);
+
+		scroll.setBounds(50, 400, 455, 300);
+		scroll.setBorder(corDaBorda);
+		scroll.setBackground(corDoJlist);
+
+//		fazendo o botao funcionar
 		buttonCalcular.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
 				Tabuada tabuada = new Tabuada();
-				tabuada.multiplicando = textFieldMultiplicando.getText();
-				tabuada.minimoMultiplicador = Double.parseDouble(textFieldMinimoMultiplicador.getText());
-				tabuada.maximoMultiplicador = Double.parseDouble(textFieldMaximoMultiplicador.getText());
+				tabuada.multiplicando = Integer.parseInt(textFieldMultiplicando.getText());
+				tabuada.minimoMultiplicador = Integer.parseInt(textFieldMinimoMultiplicador.getText());
+				tabuada.maximoMultiplicador = Integer.parseInt(textFieldMaximoMultiplicador.getText());
 
-				labelResultado.setText(labelResultado());
+				String[] resultado = tabuada.getTabuada();
+				lista.setListData(resultado);
+
 			}
 		});
-		
-		
-//		botão limpar
+
 		buttonLimpar.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Tabuada tabuada = new Tabuada();
-				
-				
+
+				textFieldMultiplicando.setText("");
+				textFieldMinimoMultiplicador.setText("");
+				textFieldMaximoMultiplicador.setText("");
+
 			}
 		});
 
-		
-		
-		
-		
-		
-		
 //		adicionar componentes ao painel (container)
+		painel.add(labelTitulo);
+		painel.add(labelSubtitulo);
 		painel.add(labelMultiplicando);
 		painel.add(textFieldMultiplicando);
 		painel.add(labelMinimoMultiplicador);
@@ -134,11 +165,10 @@ public class FrameTabuada {
 		painel.add(labelMaximoMultiplicador);
 		painel.add(textFieldMaximoMultiplicador);
 		painel.add(labelResultado);
-		painel.add(buttonLimpar);
 		painel.add(buttonCalcular);
-		
+		painel.add(buttonLimpar);
+		painel.add(scroll);
 
 		tela.setVisible(true);
 	}
 }
-	
--- src/br/senai/sp/jandira/model/AppTabuada.java
+++ src/br/senai/sp/jandira/model/AppTabuada.java
@@ -3,6 +3,7 @@ package br.senai.sp.jandira.model;
 import java.awt.Color;
 import java.awt.Font;
 
+import javax.swing.border.LineBorder;
 import br.senai.sp.jandira.gui.FrameTabuada;
 
 public class AppTabuada {
@@ -11,15 +12,23 @@ public class AppTabuada {
 		
 		FrameTabuada tela = new FrameTabuada();
 		tela.titulo = "Tabuada";
-		tela.altura = 600;
-		tela.largura = 600;
-		tela.corDeFundoDaTela = new Color(196, 196, 255);
-		tela.corDoBotaoCalcular = new Color(0, 128, 192);
-		tela.corDoBotaoLimpar = new Color(0, 128, 192);
-		tela.fonteDosLabels = new Font("Malgun Gothic", Font.PLAIN, 12);
-		tela.fonteDoTitulo = new Font("Malgun Gothic", Font.BOLD, 16);
+		tela.altura = 765;
+		tela.largura = 648;
+		tela.corDeFundoDaTela = new Color(255, 255, 255);
+		tela.corDoJlist = new Color(255, 0, 0);
+		tela.corDoBotaoCalcular = new Color (112,173,71);
+		tela.corDoBotaoLimpar = new Color(255,192,0);
+		tela.fonteDosLabels = new Font("Malgun Gothic", Font.PLAIN, 18);
+		tela.fonteDosBotoes = new Font("Malgun Gothic", Font.BOLD, 20);
+		tela.fonteDoTitulo = new Font("Malgun Gothic", Font.BOLD, 31);
+		tela.corDoTitulo = new Color(255, 0, 0);
+		tela.corDoSubtitulo = new Color(105, 105, 105);
 		tela.corDoTexto = new Color(0, 0, 0);
+		tela.corDoTextoBotao = new Color(255, 255, 255);
+		tela.corDaBorda = new LineBorder(Color.BLUE, 2);
 		tela.criarTela();
+		
+		
 
 	}
 
